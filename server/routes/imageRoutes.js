042685c0775418845703a90b43846be2dd2def14
Tejas Pakhale <119836973+codezyman@users.noreply.github.com>
2025-07-09 23:55:23 +0530

    Update imageRoutes.js

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -3,6 +3,7 @@ import { generateImage } from "../controllers/imageController.js";
 
 import userAuth from "../middlewares/auth.js";
 
+
 const imageRouter = express.Router();
 
 imageRouter.post("/generate-image", userAuth, generateImage);
